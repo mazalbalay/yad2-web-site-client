85105eeb31b8b8c0ef3c9d493acbae34eeb504e4
mazal balay <mazalbalay3@gmail.com>
2023-01-31 16:07:16 +0200

    update

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -35,7 +35,7 @@ export default function Edit(props) {
       <h1 className="text-2xl font-bold my-6 capitalize md:pl-6 pl-16">
         Edit {option}: <span className="font-normal text-2xl">{props.id}</span>
       </h1>
-      <div className="flex flex-col md:w-1/3 m-auto justify-center ">
+      <div className="flex flex-col md:w-1/3 w-2/3 m-auto justify-center ">
         first name:
         <input
           onChange={(e) => setObj({ ...obj, fName: e.target.value })}
